e4d174246b2bcde27b2f723745730282bd793afd
lgq <1241500152@qq.com>
2021-01-10 12:32:38 +0800

    P-20210110_初始化微服务架构

---
+++ springbootexmple/src/main/java/cn/lgq/springbootexmple/SpringbootexmpleApplication.java
@@ -0,0 +1,13 @@
+package cn.lgq.springbootexmple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexmpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootexmpleApplication.class, args);
+	}
+
+}
---
+++ springbootexmple/src/test/java/cn/lgq/springbootexmple/SpringbootexmpleApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lgq.springbootexmple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexmpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
